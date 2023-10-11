d5dd8fb2d1678f359770429d34706a6b2260576d
YuMZyX <joni1koskinen@gmail.com>
2023-10-11 21:19:11 +0300

    .

--- patientor/.eslintrc.cjs
+++ patientor/.eslintrc.cjs
@@ -3,10 +3,8 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -28,10 +26,4 @@ module.exports = {
     ],
     "no-case-declarations": "off"
   },
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  }
 }
