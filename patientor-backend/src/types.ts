beb047e1ebabe79701fb7f0158392a12af885ca4
YuMZyX <joni1koskinen@gmail.com>
2023-10-14 16:39:04 +0300

    Lisätty tehtävät 9.22 - 9.23

--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -1,5 +1,8 @@
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 export type NewPatient = Omit<Patient, 'id'>;
+export type Entry = HealthCheckEntry | HospitalEntry | OccupationalHealthcareEntry;
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
 
 export enum Gender {
   Male = 'male',
@@ -7,8 +10,41 @@ export enum Gender {
   Other = 'other',
 }
 
-export interface Entry {
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
 
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  }
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  }
 }
 
 export interface Diagnosis {
